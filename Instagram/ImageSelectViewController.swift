494b30ab86ec10d44864b98dfd99da8f49640a4b
TANHotmail <ttt93764672976394112233@hotmail.com>
2016-03-28 08:49:10 +0900

    Set Viewcontroler

---
+++ Instagram/ImageSelectViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ImageSelectViewController.swift
+//  Instagram
+//
+//  Created by TanakaShunichi on 2016/03/28.
+//  Copyright © 2016年 shunichi.tanaka. All rights reserved.
+//
+import UIKit
+
+class ImageSelectViewController: UIViewController {
+
+}
---
+++ Instagram/LoginViewController.swift
@@ -0,0 +1,12 @@
+//
+//  LoginViewController.swift
+//  Instagram
+//
+//  Created by TanakaShunichi on 2016/03/28.
+//  Copyright © 2016年 shunichi.tanaka. All rights reserved.
+//
+
+import UIKit
+class LoginViewController: UIViewController {
+
+}
