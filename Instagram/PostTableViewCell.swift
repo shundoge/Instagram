0f8c86737a743807940ffff71a9f2636fcb185bd
TANHotmail <ttt93764672976394112233@hotmail.com>
2016-04-01 08:43:32 +0900

    Complete

--- Instagram/PostTableViewCell.swift
+++ Instagram/PostTableViewCell.swift
@@ -11,9 +11,9 @@ class PostTableViewCell: UITableViewCell {
     
     @IBOutlet weak var postImageView: UIImageView!
     @IBOutlet weak var likeButton: UIButton!
-    @IBOutlet weak var likeLabel: UILabel!
     @IBOutlet weak var captionLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var likeLabel: UILabel!
     
     var postData: PostData?
     
